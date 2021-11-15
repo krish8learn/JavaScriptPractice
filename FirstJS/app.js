68f33799b13257a8ef814df1b1708b433dd411b4
krish-17-git <krishkarmakar17@aol.com>
2021-11-15 23:48:13 +0530

    kanban code and basic code

---
+++ FirstJS/app.js
@@ -0,0 +1,8 @@
+document.querySelectorAll('.btn').forEach((item)=>{
+    item.addEventListener('click',() =>{
+        alert("button clicked")
+    })
+})
+document.write({name:'krish'})
+console.log('torres')
+document.write("look here")
